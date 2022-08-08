013a7d5c90dc0a0925cde1bd617673c26ffc468b
Denetrah <dntrholloman@gmail.com>
2022-08-07 23:43:06 -0500

    completed assesment

---
+++ runtime.js
@@ -0,0 +1,105 @@
+const perf = require('execution-time')();
+
+
+function doublerAppend(nums){
+
+    let new_nums = [];
+
+    for (let i=0; i<nums.length; i++){
+        let num = nums[i] * 2;
+        new_nums.push(num);
+    }
+
+}
+
+
+function doublerInsert(nums){
+
+    let new_nums = [];
+
+    for (let i=0; i<nums.length; i++){
+        let num = nums[i] * 2;
+        new_nums.unshift(num);
+    }
+
+}
+
+
+function getSizedArray(size){
+    let array = [];
+    for (let i=0; i<size; i++){
+        array.push(i);
+    }
+    return array
+}
+
+
+const tinyArray = getSizedArray(10);
+const smallArray = getSizedArray(100);
+const mediumArray = getSizedArray(1000);
+const largeArray = getSizedArray(10000);
+const extraLargeArray = getSizedArray(100000);
+
+
+
+// How long does it take to double every number in a given 
+// array? 
+
+// Try it with first function
+perf.start();                     // Starts timer
+doublerAppend(extraLargeArray);
+let resultsAppend = perf.stop();  // Stops timer and save time results
+
+
+// Try it with second function
+perf.start();
+doublerInsert(extraLargeArray);
+let resultsInsert = perf.stop();
+
+
+console.log('Results for the extraLargeArray');
+console.log("insert", resultsInsert.preciseWords);
+console.log("append", resultsAppend.preciseWords);
+
+
+// tinyArray results
+
+// insert 17.959 μs
+
+// append 72.542 μs
+
+
+
+
+// smallArray results
+
+// insert 24.708 μs
+
+// append 59.375 μs
+
+
+
+
+// mediumArray results
+
+// insert 118.5 μs
+
+// append 87.125 μs
+
+
+
+
+// largeArray results
+
+// insert 6.346 ms
+
+// append 408.584 μs
+
+
+
+
+// extraLargeArray results
+
+// insert 767.322 ms
+
+// append 2.310208 ms
